631f122275abaf1bf53242ad1b58e07a32cf679e
estoyan <stoyanchik1001@gmail.com>
2024-08-15 17:31:35 +0300

    decoupled app manager logic from an app delegate and moved to a new file

--- FlickrPhotos/AppManager.swift
+++ FlickrPhotos/AppManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+final class AppManager {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow?) {
+        self.window = window ?? UIWindow(frame: UIScreen.main.bounds)
+    }
+    func presentPhotosViewController() {
+        let cache = ImagesLocalCache(countLimit: 100, totalCostLimit: 1024*1024*100)
+        let buffer = ImageRequestsBufferProvider<ImageRequest>()
+        let imageLoadingManager = ImagesLoadingManager(buffer: buffer, cache: cache)
+        let networkManager = PhotosNetworkManager()
+        let storage = PhotosSearchTermsHistoryLocalStorage()
+        let viewModel = PhotosViewModel(networkManager: networkManager,
+                                        imageRequestsManager: imageLoadingManager,
+                                        imageRequestHTTPClient: URLSession.shared,
+                                        photosSearchTermsHistoryLocalStorage: storage)
+        let photosViewController: PhotosViewController = .loadFromNib()
+        photosViewController.viewModel = viewModel
+        let navigationController = UINavigationController(rootViewController: photosViewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
